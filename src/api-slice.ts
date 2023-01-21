555b1f39ebbf6d3a3367da76a8e4e4ef10614d3e
Dominique Klevinghaus <adk@codekommando.de>
2023-01-21 15:13:40 +0100

    Update api slice

--- src/api-slice.ts
+++ src/api-slice.ts
@@ -1,14 +1,14 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
-import { Absence, Member } from './types'
+import { Absence, ApiResult, Member } from './types'
 
 export const apiSlice =  createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:8000/api/v1/' }),
   endpoints: (builder) => ({
-    getMembers: builder.query<Member[], number|void>({
+    getMembers: builder.query<ApiResult<Member[]>, number|void>({
       query: () => 'members',
     }),
-    getAbsences: builder.query<Absence[], number|void>({
+    getAbsences: builder.query<ApiResult<Absence[]>, number|void>({
       query: () => 'absences',
     }),
   }),
