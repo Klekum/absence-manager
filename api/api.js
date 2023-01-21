e60ee2a16bf3ffebf7371125e75e1904ebf90b6d
Dominique Klevinghaus <adk@codekommando.de>
2023-01-21 14:11:46 +0100

    Add API server at port 8000

--- api/api.js
+++ api/api.js
@@ -1,5 +1,8 @@
-import fs from 'fs';
-import path from 'path';
+var express = require('express')
+var app = express()
+var cors = require('cors')
+var fs = require('fs')
+var path = require('path')
 
 const ABSENCES_PATH = path.join(__dirname, 'json_files', 'absences.json');
 const MEMBERS_PATH = path.join(__dirname, 'json_files', 'members.json');
@@ -8,6 +11,11 @@ const readJsonFile = (path) => new Promise((resolve) => fs.readFile(path, 'utf8'
   .then((data) => JSON.parse(data))
   .then((data) => data.payload);
 
-export const members = () => readJsonFile(MEMBERS_PATH);
-export const absences = () => readJsonFile(ABSENCES_PATH);
+const members = () => readJsonFile(MEMBERS_PATH);
+const absences = () => readJsonFile(ABSENCES_PATH);
 
+
+app.get('/api/v1/members.json', (req, res) => { res.send(members()) })
+app.get('/api/v1/absences.json', (req, res) => { res.send(absences()) })
+
+var server = app.listen(8000, '0.0.0.0', function () {})
